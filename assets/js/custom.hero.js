72fec71dc24e318336aa52229ec6f76fd45b24c7
Munir Mahmud <munirmahmud6@gmail.com>
2019-12-28 16:54:55 +0600

    my portfolio project done

---
+++ assets/js/custom.hero.js
@@ -0,0 +1,6 @@
+(function(){
+    "use strict";
+    if( document.getElementsByClassName("full-screen").length ) {
+        document.getElementsByClassName("full-screen")[0].style.height = window.innerHeight + "px";
+    }
+})();
